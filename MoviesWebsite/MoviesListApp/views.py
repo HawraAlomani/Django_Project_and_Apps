5479479556f798673fcb9234412af053a1c786de
Hawra Alomani <ha0073@mix.wvu.edu>
2022-05-23 12:13:57 +0300

    django lab1

--- MoviesWebsite/MoviesListApp/views.py
+++ MoviesWebsite/MoviesListApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, "base.html")
+
+
+def search(request):
+    search_word = request.GET.get("search_word") or "Harry potter"
+    return render(request, "search.html", {"search_word": search_word})
--- MoviesWebsite/MoviesWebsite/settings.py
+++ MoviesWebsite/MoviesWebsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ReviewsApp',
-    'MoviesListApp'
+    'MoviesListApp',
 ]
 
 MIDDLEWARE = [
